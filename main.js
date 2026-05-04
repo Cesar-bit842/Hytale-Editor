07e0c30a780eea65299db830362960642948daea
LastKnight <dyber132@gmail.com>
2026-05-04 14:03:45 -0600

    Initiliazing

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow, Menu  } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,  
+    },
+    icon: path.join(__dirname, 'Core/Hytale Editor.png')
+  });
+
+  win.loadFile('Hytale Editor (Open Source)/index.html');
+  Menu.setApplicationMenu(null);
+}
+
+app.whenReady().then(createWindow);
